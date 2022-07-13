58d0b4fb6a0bb23f372999e44bb0fa57b9de516f
Taemin Kim <taeminkimny@gmail.com>
2022-07-13 17:35:26 -0400

    updated controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,39 @@ class ApplicationController < Sinatra::Base
     game = Game.all
     game.to_json
   end
-get "/user" do
-  user = User.all
-  user.to_json
-end
+  get "/user" do
+    user = User.all
+    user.to_json
+  end
 
-get "/comment" do
-  comment = Comment.all
-  comment.to_json
-end
+  post "/user" do
+    user = User.create(user_name: params[:user_name], password: params[:password])
+  end
+
+  get "/comment" do
+    comment = Comment.all
+    comment.to_json
+  end
+
+  get "/comment/:id" do
+    comment = Comment.find(params[:id])
+    comment.to_json
+  end
+
+  post "/comment" do
+    comment = Comment.create(body: params[:body], rating: params[:rating], game_id: params[:game_id], user_id: params[:user_id])
+    comment.to_json
+  end
+
+  patch "/comment/:id" do
+    comment = Comment.find(params[:id])
+    comment.update(body: params[:body])
+    comment.to_json
+  end
+
+  delete "/comment/:id" do
+    comment = Comment.find(params[:id])
+    comment.destroy 
+    comment.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,35 @@
+# Delete old data
+User.destroy_all
+Game.destroy_all
+Comment.destroy_all
+
 puts "Seeding users..."
 
-      User.create(user_name: "kevin" , password: "tech" )
-      User.create(user_name: "leslie", password: "flat iron")
+u1 = User.create(user_name: "kevin" , password: "tech" )
+u2 = User.create(user_name: "leslie", password: "flat iron")
 
 
 
 puts "Seeding games..."
-Game.create(prompt: "what is the opposite of a dog?", answers: "cat")
-Game.create(prompt: "what do you wear to the beach?", answers: "hat")
-Game.create(prompt: "what does a ghost say?", answers: "boo")
-Game.create(prompt: "what is the opposite of small?", answers: "big")
+g1 = Game.create(prompt: "what is the opposite of a dog?", answers: "cat")
+g2 = Game.create(prompt: "what do you wear to the beach?", answers: "hat")
+g3 = Game.create(prompt: "what does a ghost say?", answers: "boo")
+g4 = Game.create(prompt: "what is the opposite of small?", answers: "big")
+g5 = Game.create(prompt: "what is the opposite of hi?", answers: "bye")
+g6 = Game.create(prompt: "what is the opposite of null?", answers: "all")
+g7 = Game.create(prompt: "what is the opposite of foo?", answers: "boo")
+g8 = Game.create(prompt: "what is the opposite of wow?", answers: "woo")
+g9 = Game.create(prompt: "what is the opposite of bottom?", answers: "top")
+g10 = Game.create(prompt: "what is the opposite of non?", answers: "all")
 
 
 
 puts "Seeding comments..."
 
-Comment.create(body:"nice", rating: 4, game_id: 7, user_id: 4)  
-Comment.create(body:"cool", rating: 3, game_id: 6, user_id: 3)
-Comment.create(body:"great", rating: 2, game_id: 5, user_id: 2)
-Comment.create(body:"amazing", rating: 1, game_id: 4, user_id: 1)
+# Comment.create(body:"nice", rating: 4, game_id: 7J, user_id: 4)  
+# Comment.create(body:"cool", rating: 3, game_id: 6, user_id: 3)
+Comment.create(body:"great", rating: 2, game_id: g1.id, user_id: u1.id)
+Comment.create(body:"amazing", rating: 1, game_id: g2.id, user_id: u2.id)
 
 
 
